c5d9364b6666907061617c0f2447f41eb5f3bff3
d-perado <sjm01065@naver.com>
2025-10-23 16:22:55 +0900

    lv2 MenuItem 클래스 생성. (String 이름,double 가격,String 설명) App에 List선언하여 MenuItem 추가

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -6,13 +8,23 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         boolean isKiosk = true;
+
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+        menuItems.add(new MenuItem("MinSang'sBurger", 110.0, "민상님의 비밀레시피로 만들어진 특제버거"));
         while (isKiosk) {
             System.out.println("[ SHAKESHACK MENU ]");
-            System.out.println("1.\tShackBurger\t|\tW 6.9\t|\t토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-            System.out.println("2.\tSmokeShack\t|\tW 8.9\t|\t베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-            System.out.println("3.\tCheeseburger\t|\tW 6.9\t|\t포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-            System.out.println("4.\tHamburger\t|\tW 5.4\t|\t비프패티를 기반으로 야채가 들어간 기본버거");
-            System.out.println("0.\t종료\t|\t종료");
+            
+            //메뉴 출력
+            int i = 1; //메뉴 앞 숫자
+            for(MenuItem menuItem : menuItems){
+                System.out.printf("%2d. %-20s| W%-5.1f|\t%-5s\n",i++,menuItem.name
+                        ,menuItem.price
+                        ,menuItem.information);
+            }
 
             System.out.println("메뉴를 선택하세요");
 
---
+++ src/MenuItem.java
@@ -0,0 +1,17 @@
+public class MenuItem {
+
+    //속성
+    String name;
+    double price;
+    String information;
+
+    //생성자
+    MenuItem(String name, double price, String information){
+        this.name = name;
+        this.price = price;
+        this.information = information;
+    }
+
+    //기능
+
+}
