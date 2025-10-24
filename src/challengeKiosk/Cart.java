183489bec3d0e85c0f676dfb87cc562cf50e8894
d-perado <sjm01065@naver.com>
2025-10-24 18:26:48 +0900

    카트 상품 한개 삭제 갯수에따라 키값까지 제거

--- src/challengeKiosk/Cart.java
+++ src/challengeKiosk/Cart.java
@@ -1,11 +1,13 @@
 package challengeKiosk;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Cart {
     //속성
-    private List<MenuItem> selectedItems = new ArrayList<>();
+    private Map<MenuItem,Integer> selectedItems = new HashMap<>();
 
 
 
@@ -15,27 +17,44 @@ public class Cart {
     //기능
     //게터
     //선택한 물품 반환
-    public List<MenuItem> getSelectedItems() {
-        return new ArrayList<>(this.selectedItems);
+    public HashMap<MenuItem,Integer> getSelectedItems() {
+        return new HashMap<>(this.selectedItems);
     }
 
     //세터
-    public void addSelectedItem(MenuItem menuItem){
-        selectedItems.add(menuItem);
+
+    // 상품 갯수하나 추가
+    public void add(MenuItem menuItem){
+        if (!selectedItems.containsKey(menuItem)) {
+            selectedItems.put(menuItem,1);
+        } else {
+            selectedItems.put(menuItem,selectedItems.get(menuItem)+1);
+        }
+    }
+
+    // 상품 i개 추가
+    public void add(MenuItem menuItem,int i){
+        if (!selectedItems.containsKey(menuItem)) {
+            selectedItems.put(menuItem,i);
+        } else {
+            selectedItems.put(menuItem, selectedItems.get(menuItem)+i);
+        }
     }
 
     //하나의 물품 삭제
-    public void removeItem(int i) {
-        if(selectedItems.size() > i){
-            selectedItems.remove(i);
+    public void removeItem(MenuItem menuItem) {
+        if (selectedItems.containsKey(menuItem)) {
+            if (selectedItems.get(menuItem) < 2) {
+                selectedItems.remove(menuItem);
+            } else {
+                selectedItems.put(menuItem,selectedItems.get(menuItem)-1);
+            }
         }
     }
 
     //장바구니 비우기
     public void clearItem(){
-        if(!selectedItems.isEmpty()){
             selectedItems.clear();
-        }
     }
 
 }
